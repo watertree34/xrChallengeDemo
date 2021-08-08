ce01dae0287ff2850888280ab83c1b3714be7e56
watertree34 <peatedeok759@naver.com>
2021-08-09 04:34:28 +0900

    scrolladd

--- mrtkTutorial/Assets/Scripts/ResultScript.cs
+++ mrtkTutorial/Assets/Scripts/ResultScript.cs
@@ -12,8 +12,8 @@ public class ResultScript : MonoBehaviour
     private void Start()
     {
         name.GetComponent<TextMesh>().text=SceneChange.Instance.nowExerciseName(SceneChange.Instance.nowExerciseIndex);
-        time.GetComponent<TextMesh>().text = Mathf.Round(SceneChange.Instance.playTime).ToString();
-        cal.GetComponent<TextMesh>().text = Mathf.Round(SceneChange.Instance.playTime*0.57f).ToString();  // 임의로 설정
+        time.GetComponent<TextMesh>().text = Mathf.Round(SceneChange.Instance.playTime).ToString() + " 초";
+        cal.GetComponent<TextMesh>().text = Mathf.Round(SceneChange.Instance.playTime*0.57f).ToString() + " kcal";  // 임의로 설정
     }
 
     public void HomeBtn()
